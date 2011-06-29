219b33ef279988cb7aa1548baa8bda50bff79c36
Mike Zupan <mzupan@brightroll.com>
2011-06-29 11:18:34 -0700

    first import of parallel running

--- shell.py
+++ shell.py
@@ -2,27 +2,62 @@ from fabric.decorators import hosts
 from fabric.context_managers import settings
 from fabric.api import *
 
-from Queue import Queue
-
-import threading
-import readline
+from multiprocessing import Process
 
 def shell():
     FabricShell().run()
     
-class FabricShellThread(threading.Thread):
-    def __init__(self, host, cmd):
-        self.host = host
-        self.cmd = cmd
+class FabricShellQueue(object):
+    def __init__(self, max):
+        self._max = max
         
-        threading.Thread.__init__(self)
+        self._list = []
+        self._running = []
+        self._done = []
         
-    def run(self):
-        pass
+        self._finished = False
     
+    def __len__(self):
+        return len(self._list)
     
+    def append(self, obj):
+        self._list.append(obj)
+        
+    def start(self):
+        while len(self._running) < self._max:
+            self._run_process()
+                
+        while not self._finished:
+            while len(self._running) < self._max and len(self._list) > 0:
+                self._run_process()
+                
+            for id, job in enumerate(self._running):
+                if not job.is_alive():
+                    done = self._running.pop(id)
+                    self._done.append(done)
+                    
+        
+        
+            if len(self._running) == 0 and len(self._list) == 0:
+                self._finished = True
+
+    def _run_process(self):
+        job = self._list.pop()
+        env.host_string = env.host = job.name
+        job.start()
+        
+        self._running.append(job)
+
+def run_multi(host, cmd, opt):
+    with settings(host_string = host):
+        if opt == "sudo":
+            sudo(cmd)
+        else:
+            run(cmd)
+            
 class FabricShell(object):
     prompt = 'fabric::> '
+    hostlist = []
         
     def run(self):
         while True:
@@ -35,11 +70,11 @@ class FabricShell(object):
                 continue
             elif line[0] == ".":
                 parts = line.split()
+
                 if len(parts) == 1:
                     continue
-                
-                if parts[0] == ".sudo":
-                    self.run_cmd(line.replace(".sudo", ""), opt="sudo")
+                elif parts[0] == ".sudo" or parts[0] == ".msudo" or parts[0] == ".mrun":
+                    self.run_cmd(line.replace(parts[0], ""), opt=parts[0][1:])
                 
             elif line.strip() == "quit" or line.strip() == "exit":
                 break
@@ -48,14 +83,47 @@ class FabricShell(object):
 
 
     def run_cmd(self, cmd, **kwargs):
+        #
+        # here to see if we need to cleanup thread output
+        #
+        threaded = False
+        jobs = FabricShellQueue(3)
+        
         env.user = env.local_user
 
         for host in env.hosts:
             with settings(host_string = host):
-                try:
+                #try:
                     if kwargs.has_key('opt') and kwargs['opt'] == "sudo":
                         sudo(cmd)
+                    elif kwargs.has_key('opt') and kwargs['opt'] == "msudo":
+                        
+                        if env.password is None:
+                            import getpass
+                            #
+                            # set the password in the env since it's UGLY to
+                            # let fabric handle this if it spawns out a ton
+                            # of threads for hosts
+                            #
+                            env.password = getpass.getpass("Sudo password: ")
+
+                        p = Process(target=run_multi, args=(host, cmd, "sudo"))
+                        p.name = host
+                        jobs.append(p)
+                        
+                        threaded = True
+                    elif kwargs.has_key('opt') and kwargs['opt'] == "mrun":
+                        p = Process(target=run_multi, args=(host, cmd, "run"))
+                        p.name = host
+                        jobs.append(p)
+                        
+                        threaded = True
+                        
                     else:
                         run(cmd)
-                except:
-                    print "ERROR!!!!", env.host_string
+                #except:
+                #    print "ERROR!!!!", env.host_string
+                
+        if threaded:
+            jobs.start()
+                
